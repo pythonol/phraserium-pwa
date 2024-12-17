154c585ddb3c5201d0dbc1d53b7017023252cf8c
vitaly <vitaly@localhost.localdomain>
2024-12-17 12:02:43 +0300

    Init 2

--- assets/QToolbarTitle-4txSrzvC.js
+++
@@ -1,1 +0,0 @@
-import{c as t,a,h as l,b as r}from"./index-CPamuJe7.js";const n=t({name:"QToolbarTitle",props:{shrink:Boolean},setup(o,{slots:s}){const e=a(()=>"q-toolbar__title ellipsis"+(o.shrink===!0?" col-shrink":""));return()=>l("div",{class:e.value},r(s.default))}});export{n as Q};
--- assets/use-quasar-BQKbH9nT.js
+++
@@ -1,1 +0,0 @@
-import{i as a,a$ as r}from"./index-CPamuJe7.js";function u(){return a(r)}export{u};
