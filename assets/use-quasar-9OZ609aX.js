2c14842050a3d7a7d1cc79665757916697cd1ae1
vitaly <vitaly@localhost.localdomain>
2024-12-21 16:23:32 +0300

    build

--- assets/use-quasar-9OZ609aX.js
+++
@@ -1,1 +0,0 @@
-import{i as a,b3 as r}from"./index-Bwx6DDw7.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar-D3kJBd4U.js
@@ -0,0 +1,1 @@
+import{i as a,b3 as r}from"./index-1ugPO4XP.js";function u(){return a(r)}export{u};
