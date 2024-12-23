536959b7fa3cedf4a997cfabbea94bf2860c6be2
vitaly <vitaly@localhost.localdomain>
2024-12-23 14:08:08 +0300

    build

---
+++ assets/use-quasar-CSZl2uBa.js
@@ -0,0 +1,1 @@
+import{i as a,b5 as r}from"./index-CRDCpTqf.js";function u(){return a(r)}export{u};
--- assets/use-quasar-ZYSYeq5Y.js
+++
@@ -1,1 +0,0 @@
-import{i as a,b4 as r}from"./index-DtWtJiKx.js";function u(){return a(r)}export{u};
