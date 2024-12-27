f1f9b4992be250e9feca2698b1c8cdecb233cd9d
vitaly <vitaly@localhost.localdomain>
2024-12-27 16:51:55 +0300

    build

---
+++ assets/use-quasar-B-ucgDWP.js
@@ -0,0 +1,1 @@
+import{i as a,bb as r}from"./index-DFQfSLrx.js";function u(){return a(r)}export{u};
--- assets/use-quasar-B3CuJLe2.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bb as r}from"./index-CvaR7-2m.js";function u(){return a(r)}export{u};
