fff8c9ba11830793441187d3b3abfeb2a1ef55d7
vitaly <vitaly@localhost.localdomain>
2024-12-27 15:16:46 +0300

    build

--- assets/constants-DIN0dvvZ.js
+++
@@ -1,1 +0,0 @@
-const o=({app:t})=>{t.config.globalProperties.$constants={API_URL:"https://api.example.com",APP_NAME:"My App"}};export{o as default};
--- assets/use-quasar-DoQQ4wn9.js
+++
@@ -1,1 +0,0 @@
-import{i as a,ba as r}from"./index-jjrL_bQf.js";function u(){return a(r)}export{u};
