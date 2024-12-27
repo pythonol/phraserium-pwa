553a8832309f1d23a8fd0bc9226c39210013a412
vitaly <vitaly@localhost.localdomain>
2024-12-27 16:41:39 +0300

    build

--- assets/use-quasar-BkbKNZKo.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bb as r}from"./index-BViGOcTk.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar-GBw2c_an.js
@@ -0,0 +1,1 @@
+import{i as a,bb as r}from"./index-v32IrOrT.js";function u(){return a(r)}export{u};
