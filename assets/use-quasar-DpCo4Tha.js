040b765a007b3de7b3d853d37095ad4204cdc316
vitaly <vitaly@localhost.localdomain>
2024-12-27 18:45:21 +0300

    build

---
+++ assets/use-quasar-DpCo4Tha.js
@@ -0,0 +1,1 @@
+import{i as a,bb as r}from"./index-CWp_PmUG.js";function u(){return a(r)}export{u};
--- assets/use-quasar-XOjriNjG.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bb as r}from"./index-BIenFMo7.js";function u(){return a(r)}export{u};
